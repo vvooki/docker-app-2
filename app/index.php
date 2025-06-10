4f5f7efa659f0ea0f434be396b67d653c86b4a87
Lukasz <vvooki99@gmail.com>
2025-06-10 18:02:05 +0200

    update index.php to version 2

--- app/index.php
+++ app/index.php
@@ -6,6 +6,7 @@ $pass = 'testpass';
 try {
     $pdo = new PDO($dsn, $user, $pass);
     $stmt = $pdo->query("SELECT id, name FROM users");
+    echo "<h1>WERSJA 2</h1>"
     while ($row = $stmt->fetch()) {
         echo $row['id'] . ": " . $row['name'] . "\n";
     }
