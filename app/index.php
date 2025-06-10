3b2446188a82283c4765aa820a3f198f59997851
Lukasz <vvooki99@gmail.com>
2025-06-10 19:58:29 +0200

    add next layer to nginx dockerfile

--- app/index.php
+++ app/index.php
@@ -1,3 +1,7 @@
+<html>
+    <h1>WERSJA 2</h1>
+</html>
+
 <?php
 $dsn = 'mysql:host=db;dbname=testdb;charset=utf8';
 $user = 'testuser';
@@ -6,7 +10,6 @@ $pass = 'testpass';
 try {
     $pdo = new PDO($dsn, $user, $pass);
     $stmt = $pdo->query("SELECT id, name FROM users");
-    echo "<h1>WERSJA 2</h1>"
     while ($row = $stmt->fetch()) {
         echo $row['id'] . ": " . $row['name'] . "\n";
     }
